5dfd71cd2dce37f0ec650459a1d69ecdfd806c4b
premnath <premdevapp@gmail.com>
2020-12-02 18:17:30 +0530

    fragment

---
+++ app/src/main/java/com/example/mlistfragment/MyFrag.java
@@ -0,0 +1,26 @@
+package com.example.mlistfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyFrag extends Fragment {
+
+    public MyFrag() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.my_fragment, container, false);
+        return view;
+
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/mlistfragment/data/Course.java
@@ -0,0 +1,36 @@
+package com.example.mlistfragment.data;
+
+import android.content.Context;
+
+public class Course {
+    private String courseName;
+    private String courseImg;
+
+    public Course() {
+    }
+
+    public Course(String courseName, String courseImg) {
+        this.courseName = courseName;
+        this.courseImg = courseImg;
+    }
+
+    public int getImgResId(Context context){
+        return context.getResources().getIdentifier(this.courseName, "drawable", context.getPackageName());
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseImg() {
+        return courseImg;
+    }
+
+    public void setCourseImg(String courseImg) {
+        this.courseImg = courseImg;
+    }
+}
